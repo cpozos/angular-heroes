0b866a6c94dd9e1438344593e8f75d23e38ee317
“Carlos <carlosdevelopment@hotmail.com>
2024-01-02 11:59:59 -0600

    Finish Switches implementation

--- src/app/reactive-forms/pages/switches-page/switches-page.component.ts
+++ src/app/reactive-forms/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'forms-switches-page',
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class FormsSwitchesPageComponent {
 
+  public reactiveForm: FormGroup = this.builder.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]
+  });
+
+  constructor(private builder: FormBuilder) {
+  }
+
+  isNotValidField(field: string): boolean | null {
+    const control = this.reactiveForm.controls[field];
+    return control.errors && control.touched;
+  }
+
+  onSave() {
+    if (this.reactiveForm.invalid) {
+      this.reactiveForm.markAllAsTouched();
+      return;
+    }
+
+    this.reactiveForm.reset();
+  }
 }
